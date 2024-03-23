6d9e7bbc9ff24c65cd5f5f704faab523f19ee7fd
sturgeon86 <34900324+sturgeon86@users.noreply.github.com>
2024-03-22 20:45:09 -0400

    Update sensor.py Fix Indent and add Offline
    Bad indent on elif and add Offline state

--- custom_components/evduty/sensor.py
+++ custom_components/evduty/sensor.py
@@ -115,7 +115,7 @@ class EnergyConsumedSensor(EVDutyTerminalDevice, SensorEntity):
 
 class ChargingStateSensor(EVDutyTerminalDevice, SensorEntity):
     _attr_device_class = SensorDeviceClass.ENUM
-    _attr_options = ['Available', 'Charging']
+    _attr_options = ['Available', 'Charging', 'Offline']
 
     def __init__(self, coordinator: EVDutyCoordinator, terminal: Terminal) -> None:
         super().__init__(coordinator, terminal, 'State')
@@ -124,7 +124,7 @@ class ChargingStateSensor(EVDutyTerminalDevice, SensorEntity):
     def native_value(self):
         if self._terminal.status == ChargingStatus.in_use:
             return 'Charging'
-		elif self._terminal.status == ChargingStatus.out_of_service:
+	elif self._terminal.status == ChargingStatus.out_of_service:
             return 'Offline'
         return 'Available'
 
