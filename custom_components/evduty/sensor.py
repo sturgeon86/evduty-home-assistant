534ff437abc34e5de737164108b914b8b8a45f24
sturgeon86 <34900324+sturgeon86@users.noreply.github.com>
2024-03-22 20:38:18 -0400

    Add OutOfService status handling

--- custom_components/evduty/sensor.py
+++ custom_components/evduty/sensor.py
@@ -124,6 +124,8 @@ class ChargingStateSensor(EVDutyTerminalDevice, SensorEntity):
     def native_value(self):
         if self._terminal.status == ChargingStatus.in_use:
             return 'Charging'
+		elif self._terminal.status == ChargingStatus.out_of_service:
+            return 'Offline'
         return 'Available'
 
 
